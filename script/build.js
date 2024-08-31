b2c7ce481493b709f5a86d4d8cc43f6275c271b4
surunzi <surunzi@foxmail.com>
2024-08-31 21:58:07 +0800

    chore: small changes

--- script/build.js
+++ script/build.js
@@ -1,9 +1,15 @@
 const fs = require('licia/fs')
+const promisify = require('licia/promisify')
+const mkdir = promisify(require('licia/mkdir'))
 const path = require('path')
 
 async function copyDefinition() {
   const data = await fs.readFile(path.resolve(__dirname, '../src/index.d.ts'))
-  await fs.writeFile(path.resolve(__dirname, '../dist/chobitsu.d.ts'), data)
+  const dist = path.resolve(__dirname, '../dist')
+  if (!(await fs.exists(dist))) {
+    await mkdir(dist)
+  }
+  await fs.writeFile(path.resolve(dist, 'chobitsu.d.ts'), data)
 }
 
 copyDefinition()
